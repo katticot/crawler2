0bf542802873e474e1ce1590ddcd689379ce0162
keita <sagara911@gmail.com>
2017-07-26 20:08:19 +0200

    Initial commit

--- Main.py
+++ Main.py
@@ -4,10 +4,15 @@ import bs4
 
 # help(bs4)
 
+#import the library used to query a website
+import urllib2
+#specify the url
+wiki = "https://en.wikipedia.org/wiki/List_of_state_and_union_territory_capitals_in_India"
+#Query the website and return the html to the variable 'page'
+page = urllib2.urlopen(wiki)
+#import the Beautiful soup functions to parse the data returned from the website
+from bs4 import BeautifulSoup
+#Parse the html in the 'page' variable, and store it in Beautiful Soup format
+soup = BeautifulSoup(page)
 
-url_ligue_1 = "https://fr.wikipedia.org/wiki/Championnat_de_France_de_football_2016-2017"
-
-from urllib2 import Request
-request_text = Request.get_full_url(url_ligue_1).read()
-print(request_text[:1000])
-test
+print soup.prettify()
