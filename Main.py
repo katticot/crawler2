c786e16b8d98fc4ab078739f010e121378ef7c83
keita <sagara911@gmail.com>
2017-07-26 19:49:40 +0200

    Initial commit

--- Main.py
+++ Main.py
@@ -10,3 +10,4 @@ url_ligue_1 = "https://fr.wikipedia.org/wiki/Championnat_de_France_de_football_2
 from urllib2 import Request
 request_text = Request.get_full_url(url_ligue_1).read()
 print(request_text[:1000])
+test
