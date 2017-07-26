b467fc6a43387a770df17a7607cfb7ad016954eb
keita <sagara911@gmail.com>
2017-07-27 00:00:56 +0200

    Initial commit

--- Main.py
+++ Main.py
@@ -8,11 +8,18 @@ import bs4
 import urllib2
 #specify the url
 wiki = "https://en.wikipedia.org/wiki/List_of_state_and_union_territory_capitals_in_India"
+wiki2="http://www.welcometofrance.com/en/you-are-an-employee-your-employer-is-outside-france"
 #Query the website and return the html to the variable 'page'
-page = urllib2.urlopen(wiki)
+page = urllib2.urlopen(wiki2)
 #import the Beautiful soup functions to parse the data returned from the website
 from bs4 import BeautifulSoup
 #Parse the html in the 'page' variable, and store it in Beautiful Soup format
-soup = BeautifulSoup(page)
+soup = BeautifulSoup(page,"html.parser")
 
-print soup.prettify()
+#print soup.prettify()
+
+ref = soup.a
+soup.a.string
+all_tables=soup.find_all("a")
+
+print all_tables
