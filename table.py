ed042ae9997ceef7dbe0c4e1cfccada03c7ea4ab
keita <sagara911@gmail.com>
2017-07-27 19:43:05 +0200

    add table.py

---
+++ table.py
@@ -0,0 +1,23 @@
+import urllib
+
+import bs4
+
+# help(bs4)
+
+# import the library used to query a website
+import urllib2
+
+# specify the url
+wiki = "https://en.wikipedia.org/wiki/List_of_state_and_union_territory_capitals_in_India"
+wiki2 = "http://www.welcometofrance.com/en/you-are-an-employee-your-employer-is-outside-france"
+# Query the website and return the html to the variable 'page'
+page = urllib2.urlopen(wiki)
+# import the Beautiful soup functions to parse the data returned from the website
+from bs4 import BeautifulSoup
+
+# Parse the html in the 'page' variable, and store it in Beautiful Soup format
+soup = BeautifulSoup(page, "html.parser")
+all_table = soup.find_all("table")
+# print soup.prettify()
+right_table=soup.find('table', class_='wikitable sortable plainrowheaders')
+print soup.find_all("table")
